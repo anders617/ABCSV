4dae1015bd07c6565669a82daa8c945f2d2bcb20
anders617 <anders617@gmail.com>
2016-01-04 09:09:30 -0500

    Initial Commit

---
+++ ABCSV/ABCSV.h
@@ -0,0 +1,19 @@
+//
+//  ABCSV.h
+//  ABCSV
+//
+//  Created by Anders Boberg on 1/4/16.
+//  Copyright © 2016 Anders boberg. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ABCSV.
+FOUNDATION_EXPORT double ABCSVVersionNumber;
+
+//! Project version string for ABCSV.
+FOUNDATION_EXPORT const unsigned char ABCSVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ABCSV/PublicHeader.h>
+
+
